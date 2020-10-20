98cd72ae18c61372c2a9a25341bb241a034966ee
kirtiJainkj <71442364+kirtiJainkj@users.noreply.github.com>
2020-10-20 17:48:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect,movingRect
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(200, 200, 50, 80);
+  movingRect=createSprite(400, 200, 80, 30);
+  fixedRect.shapeColor="green"
+  movingRect.shapeColor="green"
+
+}
+
+
+
+
+function draw() {
+  background(0,0,0,);  
+  movingRect.x= mouseX
+  movingRect.y= mouseY
+  if(movingRect.x-fixedRect.x<movingRect.width/2 + fixedRect.width/2){
+
+    fixedRect.shapeColor="red"
+    movingRect.shapeColor="red"
+
+  }
+  else{
+
+    fixedRect.shapeColor="green"
+  movingRect.shapeColor="green"
+  }
+  drawSprites();
+}
